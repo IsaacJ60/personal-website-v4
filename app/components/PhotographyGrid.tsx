624af955dbf0673c3ebee8fcc5146c6fe2beed85
Isaac Jiang <isaac.jiang66@gmail.com>
2026-03-08 03:21:23 -0700

    edit photo

--- app/components/PhotographyGrid.tsx
+++ app/components/PhotographyGrid.tsx
@@ -11,7 +11,7 @@ import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 const PHOTOS: Photo[] = [
     { src: "/images/camera/AML06129.jpg", width: 4673, height: 3417, alt: "Photography shot AML05497" },
-    { src: "/images/camera/AML05832.jpg", width: 2505, height: 1548, alt: "Photography shot AML05832" },
+    { src: "/images/camera/AML05832.jpg", width: 2806, height: 1874, alt: "Photography shot AML05832" },
     { src: "/images/camera/AML05988.jpg", width: 4333, height: 2678, alt: "Photography shot AML05988" },
     { src: "/images/camera/AML06293_01.jpg", width: 5560, height: 3932, alt: "Photography shot AML05707_01" },
     { src: "/images/camera/AML06407.jpg", width: 6026, height: 4024, alt: "Photography shot AML05782" },
