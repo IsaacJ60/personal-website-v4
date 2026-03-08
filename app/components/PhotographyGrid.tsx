a382b13d18fcd9eaf5dd8748cfc69876cfe482d6
Isaac Jiang <isaac.jiang66@gmail.com>
2026-03-08 01:56:15 -0800

    update photos

--- app/components/PhotographyGrid.tsx
+++ app/components/PhotographyGrid.tsx
@@ -10,11 +10,12 @@ import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 const PHOTOS: Photo[] = [
+    { src: "/images/camera/AML06129.jpg", width: 4673, height: 3417, alt: "Photography shot AML05497" },
     { src: "/images/camera/AML05832.jpg", width: 2505, height: 1548, alt: "Photography shot AML05832" },
     { src: "/images/camera/AML05988.jpg", width: 4333, height: 2678, alt: "Photography shot AML05988" },
     { src: "/images/camera/AML06293_01.jpg", width: 5560, height: 3932, alt: "Photography shot AML05707_01" },
     { src: "/images/camera/AML06407.jpg", width: 6026, height: 4024, alt: "Photography shot AML05782" },
-    { src: "/images/camera/AML05741.jpg", width: 3878, height: 5973, alt: "Photography shot AML05741" },
+    { src: "/images/camera/AML05726.jpg", width: 5272, height: 3520, alt: "Photography shot AML05741" },
     { src: "/images/camera/AML06125_02.jpg", width: 5787, height: 3683, alt: "Photography shot AML05803" },
     { src: "/images/camera/AML05498.jpg", width: 6026, height: 4024, alt: "Photography shot AML05498" },
     { src: "/images/camera/AML05871.jpg", width: 4255, height: 2630, alt: "Photography shot AML05871" },
