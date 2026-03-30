75e77093d5af9f656c1a458073f12c63a6da8a32
Isaac Jiang <isaac.jiang66@gmail.com>
2026-03-29 21:37:40 -0700

    new photos!

--- app/components/PhotographyGrid.tsx
+++ app/components/PhotographyGrid.tsx
@@ -10,12 +10,15 @@ import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 const PHOTOS: Photo[] = [
+    { src: "/images/camera/AML09858.jpg", width: 4875, height: 3900, alt: "MOON AND CHERRY BLOSSOMS"},
+    { src: "/images/camera/AML08321_01.jpg", width: 5387, height: 3401, alt: "COUPLE DURING SUNSET"},
+    { src: "/images/camera/AML08369_06.jpg", width: 3600, height: 2880, alt: "GIRL UNDER BLOSSOMS"},
     { src: "/images/camera/AML07020_07.jpg", width: 5264, height: 3948, alt: "RAVEN CLOSE UP" },
     { src: "/images/camera/AML06293_09.jpg", width: 4240, height: 3180, alt: "CANOPY PARACHUTE" },
     { src: "/images/camera/AML06129_03.jpg", width: 4914, height: 3743, alt: "TWO UMBRELLA" },
-    { src: "/images/camera/AML06994_01.jpg", width: 5368, height: 4023, alt: "RAVEN SIDE PROFILE" },
     { src: "/images/camera/AML05828_09.jpg", width: 2744, height: 1832, alt: "MOON AND PLANE" },
     { src: "/images/camera/AML05988_02.jpg", width: 4333, height: 2678, alt: "SUNSET SILLOUETTE" },
+    { src: "/images/camera/AML09619_01.jpg", width: 4145, height: 3316, alt: "TEAL BUILDINGS" },
 ];
 
 export default function PhotographyGrid() {
