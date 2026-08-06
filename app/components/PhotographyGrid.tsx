8489853c49c61e5d373505ed1193d4b2f9e0ffe2
Isaac Jiang <isaac.jiang66@gmail.com>
2026-08-06 11:10:41 -0700

    simplify photography tab

--- app/components/PhotographyGrid.tsx
+++ app/components/PhotographyGrid.tsx
@@ -7,19 +7,12 @@ export default function PhotographyGrid() {
         <section className="photography-grid space-y-3">
             <p className="text-md uppercase text-muted-foreground tracking-wide">Photography</p>
 
-            <div className="h-32 rounded-lg bg-gray-50 dark:bg-zinc-900 flex items-center justify-center px-6">
-                <p className="text-center text-muted-foreground">
-                    I love photography! I love how it can freeze any given moment in time, capture the tiniest details, and make people feel appreciated, and loved.
-                    To view my portfolio, please click the button below.
-                </p>
-            </div>
-
             <div className="mt-4 flex justify-center">
                 <Link
                     href="/photo"
                     className="inline-block rounded-md bg-neutral-900 px-5 py-2 text-sm font-semibold text-white hover:bg-neutral-800 focus:outline-none focus:ring-2 focus:ring-neutral-400"
                 >
-                    Enter full viewer
+                    Gallery
                 </Link>
             </div>
         </section>
